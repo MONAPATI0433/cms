568f542097defde5302c82a12b2fb8963970075a
aslamsk <aslamsk777@gmail.com>
2020-01-16 09:03:40 +0530

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Welcome</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit a,
+      aperiam facere! Alias corporis aspernatur cupiditate consequuntur iusto
+      quibusdam error, veniam vero quia id reprehenderit doloribus est, eum
+      blanditiis eaque labore voluptate totam unde accusamus ipsam consectetur
+      delectus magni a, fuga! Dolor reprehenderit iure et maiores iste, hic
+      obcaecati vero esse, ad libero? Necessitatibus, dolorum eaque quia ipsum
+      nostrum quidem architecto dicta. Nostrum harum facere animi quos et
+      reiciendis a iste voluptatem rerum hic aut temporibus, sed, quo nisi neque
+      vel quae vitae laborum dolorum illo voluptate labore explicabo, excepturi
+      libero impedit. Minus possimus, neque cumque totam necessitatibus
+      praesentium nesciunt!
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
